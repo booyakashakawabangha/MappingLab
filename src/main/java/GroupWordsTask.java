7dbd6dfef0908d01621f829f775a468681d74576
booyakashakawabangha <tatarik.sv@gmail.com>
2021-12-22 21:37:39 +0200

    GroupWordsTask refactored

--- src/main/java/GroupWordsTask.java
+++ src/main/java/GroupWordsTask.java
@@ -1,18 +1,26 @@
 import java.util.*;
 import java.util.stream.Collectors;
 
+import static java.util.stream.Collectors.*;
+
 public class GroupWordsTask {
 
-    public static Map.Entry<String, String> transform(Map.Entry<Character, List<String>> e) {
-        return new AbstractMap.SimpleEntry<>(String.valueOf(e.getKey()), Collections.frequency(e.getValue(), e.getKey())
-                .stream().collect(Collectors.joining()));
+    public static Map.Entry<String,String> transform(Map.Entry<Character, List<String>> el){
+        return new AbstractMap
+                .SimpleEntry<>(
+                String.valueOf(el.getKey()),
+                el.getValue().stream().collect(joining()));
     }
 
     public static Map<String, String> groupIt(List<String> array) {
         return array.stream()
-                .collect(Collectors.groupingBy(x -> x.charAt(0), HashMap::new, Collectors.toList()))
-                .entrySet().stream().map(GroupWordsTask::transform).collect(Collectors.toMap(
-                        Map.Entry::getKey, Map.Entry::getValue));
-
+                .collect(groupingBy(value -> value.charAt(0), HashMap::new, toList()))
+                .entrySet()
+                .stream()
+                .map(GroupWordsTask::transform)
+                .collect(toMap(Map.Entry::getKey, Map.Entry::getValue))
+                .entrySet()
+                .stream()
+                .collect(toMap(Map.Entry::getKey, x -> x.getValue().chars().filter(y -> y == x.getKey().charAt(0)).count()));
     }
 }
